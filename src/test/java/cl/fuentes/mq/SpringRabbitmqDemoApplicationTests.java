0b83e949baa3c0e0eb589248370cdeceb1a20849
Alvaro Fuentes <alvarofue@gmail.com>
2019-02-01 16:37:58 -0300

    Inicia código

---
+++ src/test/java/cl/fuentes/mq/SpringRabbitmqDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package cl.fuentes.mq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRabbitmqDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
